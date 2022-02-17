fc7d804de1b8e83495bdfdc99a2da10f31bb998f
RoboticaIndustrial2021 <83782073+RoboticaIndustrial2021@users.noreply.github.com>
2022-02-17 09:17:15 -0500

    Update elipse.py

--- elipse.py
+++ elipse.py
@@ -2,27 +2,28 @@ from robolink import *    # RoboDK API
 from robodk import *      # Robot toolbox 
 from time import sleep
 RDK = Robolink() 
-rob = robodk 
+
  
 robot = RDK.ItemUserPick('',ITEM_TYPE_ROBOT)    #adquirir todos los parámetros d el robot 
 if not robot.Valid(): 
     quit() 
-reference = robot.Parent()  # devuelve el artículo robot.setPoseFrame(reference)#establece el marco de referencia de un robot pose_ref=robot.Pose()  #devuelve la posición actual del robot con matriz 
-robot.setPoseFrame(reference)
 
 def elipse(): 
+    frameletras = RDK.Item("FrameLetras") 
+    robot.setPoseFrame(frameletras)
     Ref1 = RDK.Item("Letras") 
     RefTar = Ref1.Pose() 
-    b1 = rob.pose_2_xyzrpw(RefTar)     
+    b1 = Pose_2_TxyzRxyz(RefTar)    
+    print (b1) 
     t = -pi/2     
     x0= b1[0]     
     y0= b1[1]     
     z0= b1[2]     
-    a=20     
-    b=10 
+    a=250     
+    b=120 
     RDK.RunProgram('WeldOn(-1)')     
-    robot.MoveJ(RefTar*roty(-pi))
-    robot.setSpeed(2050,2000)    
+    robot.MoveJ(transl(b1)*roty(-pi))
+    robot.setSpeed(5050,2000)    
     while (t <=3*pi/2):         
         pos = transl(x0 + a*cos(t), y0 + b*sin(t), z0)*roty(-pi)         
         robot.MoveJ(pos)    
