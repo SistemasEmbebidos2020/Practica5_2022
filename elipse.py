f5acacc15fb45bbdc286af05f0ba0dcae918304a
RoboticaIndustrial2021 <83782073+RoboticaIndustrial2021@users.noreply.github.com>
2022-02-19 10:26:26 -0500

    Update elipse.py

--- elipse.py
+++ elipse.py
@@ -1,5 +1,6 @@
-from robolink import *    # RoboDK API 
-from robodk import *      # Robot toolbox 
+from robodk.robolink import *  # RoboDK API
+from robodk.robomath import *
+
 from time import sleep
 RDK = Robolink() 
 
